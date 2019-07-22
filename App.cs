0961f90cd571aba87491a8d79f87d1c94a72814f
s3virge <kobzarvit@gmail.com>
2019-07-22 17:56:22 +0300

    first commit

---
+++ App.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMatrix {
+    class App {
+        public static void Main() {
+            ConsoleMatrix cm = new ConsoleMatrix();
+            cm.run();
+
+            //MatrixRain mr = new MatrixRain();
+            //mr.run();
+        }
+    }
+}
